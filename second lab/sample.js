fa6fbdfeb45e3b9ede7085ca74db41de2b784720
Yulia Botalova <botalowa.yu@yandex.ru>
2018-11-18 21:50:10 +0500

    second commit

---
+++ second lab/sample.js
@@ -0,0 +1,24 @@
+ var AWS = require('aws-sdk');
+
+ var AWS = require('aws-sdk');
+ var S3     = require('aws-sdk/clients/s3');
+
+ AWS.config.update({
+    region: 'ru-msk',
+    endpoint: 'http://hb.bizmrg.com',
+ });
+
+var s3 = new AWS.S3();
+
+s3.getObject({Bucket: 'firstbaket', Key: 'depositphotos_54081723-stock-photo-beautiful-nature-landscape.jpg'}, function(err, data){
+    if (err) console.log(err, err.stack); // an error occurred
+    else     console.log(data);           // successful response
+ });
+
+s3.putObject({
+    Bucket: 'firstbaket',
+    Key: "pic1.png",
+ }, function(err, data){
+    if (err) console.log(err, err.stack); // an error occurred
+    else     console.log(data);           // successful response
+ } );
